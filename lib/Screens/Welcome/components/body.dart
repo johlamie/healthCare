9bb07b35abe10ec4f3b32cfeeff4ef666ed6e1da
johlamie <johnlamien@gmail.com>
2021-04-18 03:19:19 +0200

    Add First Welcome Page

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,32 +1,28 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:healtcare/Screens/Welcome/components/background.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; // Donne height et width de l'écran
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-              child: Image.asset(
-                "assets/images/main_top.png", width: size.width * 0.3,
-              ),
-          ),
-          Positioned(
-            bottom: 0,
-              left: 0,
-              child: Image.asset(
-                "assets/images/main_bottom.png", width: size.width * 0.2,
-              ),
-          ),
-          Text("Bienvenue")
-        ],
-      ),
-    );
+    return Background(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "BIENVENUE DANS HEALTHCARE",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: size.height * 0.05),
+        SvgPicture.asset(
+          "assets/icons/chat.svg",
+          height: size.height * 0.45,
+        ),
+        SizedBox(height: size.height * 0.05),
+      ],
+    ));
   }
 }

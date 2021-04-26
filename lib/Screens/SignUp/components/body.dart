29428bbc046307d1e40e63c3e5e612297dcba0d4
johlamie <johnlamien@gmail.com>
2021-04-26 21:26:41 +0200

    Ajout des réseaux sociaux pour l'inscription

--- lib/Screens/SignUp/components/body.dart
+++ lib/Screens/SignUp/components/body.dart
@@ -63,8 +63,58 @@ class Body extends StatelessWidget {
               );
             },
           ),
-          OrDivider()
+          OrDivider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SocialIcon(
+                iconSrc: "assets/icons/facebook.svg",
+                press: () {},
+              ),
+              SocialIcon(
+                iconSrc: "assets/icons/twitter.svg",
+                press: () {},
+              ),
+              SocialIcon(
+                iconSrc: "assets/icons/google-plus.svg",
+                press: () {},
+              ),
+            ],
+          )
         ],
+      ),
+    );
+  }
+}
+
+class SocialIcon extends StatelessWidget {
+  final String iconSrc;
+  final Function press;
+  const SocialIcon({
+    Key key,
+    this.iconSrc,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 2,
+            color: kPrimaryLightColor,
+          ),
+          shape: BoxShape.circle,
+        ),
+        child: SvgPicture.asset(
+          iconSrc,
+          width: 20,
+          height: 20,
+        ),
       ),
     );
   }

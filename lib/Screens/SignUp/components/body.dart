33350f3716edb86a02be25d8b03095134eddff24
johlamie <johnlamien@gmail.com>
2021-04-26 20:12:43 +0200

    Ajout des composants de la page d'inscription

--- lib/Screens/SignUp/components/body.dart
+++ lib/Screens/SignUp/components/body.dart
@@ -1,7 +1,11 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:healtcare/Screens/SignUp/components/background.dart';
+import 'package:healtcare/Screens/login/login_screen.dart';
+import 'package:healtcare/components/already_have_an_account_check.dart';
+import 'package:healtcare/components/rouded_button.dart';
+import 'package:healtcare/components/rounded_input_field.dart';
+import 'package:healtcare/components/rounded_password_field.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -12,10 +16,61 @@ class Body extends StatelessWidget {
     @required this.child,
   }) : super(key: key);
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Backgroud(
       child: Column(
-        children: <Widget>[],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "S'INSCRIRE",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          SvgPicture.asset(
+            "assets/icons/signup.svg",
+            height: size.height * 0.35,
+          ),
+          RoundedInputField(
+            hintText: "Votre e-mail",
+            onChanged: (value) {},
+          ),
+          RoundedPasswordField(
+            onChanged: (value) {},
+          ),
+          RoundedButton(
+            text: "S'INSCRIRE",
+            press: () {},
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          AlreadyHavreAnAccountCheck(
+            login: false,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return LoginScreen();
+                  },
+                ),
+              );
+            },
+          ),
+          OrDivider()
+        ],
       ),
     );
+  }
+}
+
+class OrDivider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }

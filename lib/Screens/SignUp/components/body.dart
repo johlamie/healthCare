acaad28476bd26a762bcc9257e6136c00cdc8252
johlamie <johnlamien@gmail.com>
2021-04-26 21:54:11 +0200

    separe Social Link

--- lib/Screens/SignUp/components/body.dart
+++ lib/Screens/SignUp/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:healtcare/Screens/SignUp/components/background.dart';
 import 'package:healtcare/Screens/SignUp/components/or_divider.dart';
+import 'package:healtcare/Screens/SignUp/components/social_icons.dart';
 import 'package:healtcare/Screens/login/login_screen.dart';
 import 'package:healtcare/components/already_have_an_account_check.dart';
 import 'package:healtcare/components/rouded_button.dart';
@@ -11,109 +12,72 @@ import 'package:healtcare/constants.dart';
 
 class Body extends StatelessWidget {
   @override
-  final Widget child;
-
-  const Body({
-    Key key,
-    @required this.child,
-  }) : super(key: key);
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Backgroud(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "S'INSCRIRE",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "S'INSCRIRE",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SizedBox(
-            height: size.height * 0.03,
-          ),
-          SvgPicture.asset(
-            "assets/icons/signup.svg",
-            height: size.height * 0.35,
-          ),
-          RoundedInputField(
-            hintText: "Votre e-mail",
-            onChanged: (value) {},
-          ),
-          RoundedPasswordField(
-            onChanged: (value) {},
-          ),
-          RoundedButton(
-            text: "S'INSCRIRE",
-            press: () {},
-          ),
-          SizedBox(
-            height: size.height * 0.03,
-          ),
-          AlreadyHavreAnAccountCheck(
-            login: false,
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return LoginScreen();
-                  },
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+              hintText: "Votre e-mail",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "S'INSCRIRE",
+              press: () {},
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            AlreadyHavreAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            OrDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SocialIcon(
+                  iconSrc: "assets/icons/facebook.svg",
+                  press: () {},
                 ),
-              );
-            },
-          ),
-          OrDivider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SocialIcon(
-                iconSrc: "assets/icons/facebook.svg",
-                press: () {},
-              ),
-              SocialIcon(
-                iconSrc: "assets/icons/twitter.svg",
-                press: () {},
-              ),
-              SocialIcon(
-                iconSrc: "assets/icons/google-plus.svg",
-                press: () {},
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class SocialIcon extends StatelessWidget {
-  final String iconSrc;
-  final Function press;
-  const SocialIcon({
-    Key key,
-    this.iconSrc,
-    this.press,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 2,
-            color: kPrimaryLightColor,
-          ),
-          shape: BoxShape.circle,
-        ),
-        child: SvgPicture.asset(
-          iconSrc,
-          width: 20,
-          height: 20,
+                SocialIcon(
+                  iconSrc: "assets/icons/twitter.svg",
+                  press: () {},
+                ),
+                SocialIcon(
+                  iconSrc: "assets/icons/google-plus.svg",
+                  press: () {},
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/Screens/login/components/body.dart
+++ lib/Screens/login/components/body.dart
@@ -20,44 +20,46 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "LOGIN",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: size.height * 0.03),
-          SvgPicture.asset(
-            "assets/icons/login.svg",
-            height: size.height * 0.35,
-          ),
-          SizedBox(height: size.height * 0.03),
-          RoundedInputField(
-            hintText: "Identifiant",
-            onChanged: (value) {},
-          ),
-          RoundedPasswordField(
-            onChanged: (value) {},
-          ),
-          RoundedButton(
-            text: "SE CONNECTER",
-            press: () {},
-          ),
-          SizedBox(height: size.height * 0.03),
-          AlreadyHavreAnAccountCheck(
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return SignUpScreen();
-                  },
-                ),
-              );
-            },
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
+            ),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Identifiant",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "SE CONNECTER",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHavreAnAccountCheck(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }

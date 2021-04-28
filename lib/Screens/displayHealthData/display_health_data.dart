70a256caf6c6967b85e297229c66beeccd947468
johlamie <johnlamien@gmail.com>
2021-04-28 19:14:13 +0200

    Appel au Framework Health de flutter pour pouvoir lire les données de santé

--- lib/Screens/displayHealthData/display_health_data.dart
+++ lib/Screens/displayHealthData/display_health_data.dart
@@ -1,9 +1,168 @@
 import 'package:flutter/material.dart';
 import 'package:healtcare/constants.dart';
+import 'package:health/health.dart';
+import 'dart:async';
 
-class DisplayHealthData extends StatelessWidget {
+class DisplayHealthData extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+enum AppState {
+  DATA_NOT_FETCHED,
+  FETCHING_DATA,
+  DATA_READY,
+  NO_DATA,
+  AUTH_NOT_GRANTED
+}
+
+class _MyAppState extends State<DisplayHealthData> {
+  List<HealthDataPoint> _healthDataList = [];
+  AppState _state = AppState.DATA_NOT_FETCHED;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> fetchData() async {
+    /// Get everything from midnight until now
+    DateTime startDate = DateTime(2020, 11, 07, 0, 0, 0);
+    DateTime endDate = DateTime(2025, 11, 07, 23, 59, 59);
+
+    HealthFactory health = HealthFactory();
+
+    /// Define the types to get.
+    List<HealthDataType> types = [
+      HealthDataType.STEPS,
+      HealthDataType.WEIGHT,
+      HealthDataType.HEIGHT,
+      HealthDataType.BLOOD_GLUCOSE,
+      HealthDataType.DISTANCE_WALKING_RUNNING,
+    ];
+
+    setState(() => _state = AppState.FETCHING_DATA);
+
+    /// You MUST request access to the data types before reading them
+    bool accessWasGranted = await health.requestAuthorization(types);
+
+    int steps = 0;
+
+    if (accessWasGranted) {
+      try {
+        /// Fetch new data
+        List<HealthDataPoint> healthData =
+            await health.getHealthDataFromTypes(startDate, endDate, types);
+
+        /// Save all the new data points
+        _healthDataList.addAll(healthData);
+      } catch (e) {
+        print("Caught exception in getHealthDataFromTypes: $e");
+      }
+
+      /// Filter out duplicates
+      _healthDataList = HealthFactory.removeDuplicates(_healthDataList);
+
+      /// Print the results
+      _healthDataList.forEach((x) {
+        print("Data point: $x");
+        steps += x.value.round();
+      });
+
+      print("Steps: $steps");
+
+      /// Update the UI to display the results
+      setState(() {
+        _state =
+            _healthDataList.isEmpty ? AppState.NO_DATA : AppState.DATA_READY;
+      });
+    } else {
+      print("Autorisation non accordée");
+      setState(() => _state = AppState.DATA_NOT_FETCHED);
+    }
+  }
+
+  Widget _contentFetchingData() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+            padding: EdgeInsets.all(20),
+            child: CircularProgressIndicator(
+              strokeWidth: 10,
+            )),
+        Text('Récupération des données...')
+      ],
+    );
+  }
+
+  Widget _contentDataReady() {
+    return ListView.builder(
+        itemCount: _healthDataList.length,
+        itemBuilder: (_, index) {
+          HealthDataPoint p = _healthDataList[index];
+          return ListTile(
+            title: Text("${p.typeString}: ${p.value}"),
+            trailing: Text('${p.unitString}'),
+            subtitle: Text('${p.dateFrom} - ${p.dateTo}'),
+          );
+        });
+  }
+
+  Widget _contentNoData() {
+    return Text('Aucune donnée pour le moment');
+  }
+
+  Widget _contentNotFetched() {
+    return Text(
+      'Appuyez sur le bouton de Téléchargement pour afficher vos données',
+    );
+  }
+
+  Widget _authorizationNotGranted() {
+    return Text('''Autorisation non donnée.
+        Vérifiez les autorisations dans Apple Health.''');
+  }
+
+  Widget _content() {
+    if (_state == AppState.DATA_READY)
+      return _contentDataReady();
+    else if (_state == AppState.NO_DATA)
+      return _contentNoData();
+    else if (_state == AppState.FETCHING_DATA)
+      return _contentFetchingData();
+    else if (_state == AppState.AUTH_NOT_GRANTED)
+      return _authorizationNotGranted();
+
+    return _contentNotFetched();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'VOS DONNEES',
+            style: TextStyle(color: blackColor),
+          ),
+          backgroundColor: whiteColor,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.file_download),
+              onPressed: () {
+                fetchData();
+              },
+              color: blackColor,
+            )
+          ],
+        ),
+        body: Center(
+          child: _content(),
+        ),
+        // backgroundColor: blackColor,
+      ),
+    );
   }
 }

280d36dffdf5ad8a882adcdd996f10c2a0d9bc2c
johlamie <johnlamien@gmail.com>
2021-06-30 15:23:04 +0200

    Ajout des localisation de l'ESGI et test de récupération de longitude et latitude

--- lib/Screens/Home/homePage.dart
+++ lib/Screens/Home/homePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:healtcare/Screens/Drawer/drawer.dart';
 import 'package:healtcare/Screens/displayHealthData/display_health_data.dart';
 import 'package:healtcare/constants.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +13,26 @@ class _HomePageState extends State<HomePage> {
   PageController _pageController = PageController();
   List<Widget> _screens = [DisplayHealthData(), DrawerPage()];
   int _selectedIndex = 0;
+  String latitude, longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocalisation();
+  }
+
+  getCurrentLocalisation() async {
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    var lastPosition = await Geolocator.getLastKnownPosition();
+    // print(lastPosition);
+
+    setState(() {
+      print('${position.latitude}');
+      print('${position.longitude}');
+    });
+  }
 
   void _onPageChanged(int index) {
     setState(() {
